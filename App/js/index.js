a6e2735cb180f399786b3895d233ffb1fee1d762
savvito123 <46897334+savvito123@users.noreply.github.com>
2020-02-24 09:31:30 +0200

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -32,12 +32,10 @@ xui.Class('App', 'xui.Module',{
             
             append(
                 xui.create("xui.UI.Button")
-                .setHost(host,"xui_ui_button11")
+                .setHost(host,"xui_ui_button14")
                 .setDirtyMark(false)
-                .setLeft("35.833333333333336em")
-                .setTop("25em")
-                .setWidth("5.416666666666667em")
-                .setHeight("9.5em")
+                .setLeft("15em")
+                .setTop("35.833333333333336em")
                 .setCaption("Button")
             );
             
